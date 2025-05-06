88d5bdd410b1db20e7f8d8943d6462f594b1a847
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-05-06 13:47:59 +0530

    refactor: cartItemsContext with useCartItemsStore using zustand

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,17 +1,19 @@
-import { useContext } from "react";
-
 import { LeftArrow } from "neetoicons";
 import { Typography } from "neetoui";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useHistory } from "react-router-dom";
-import CartItemsContext from "src/contexts/CartItemsContext";
+import useCartItemsStore from "stores/useCartItemsStore";
 
 const Header = ({ title, shouldDisplayBackButton = false, searchAction }) => {
   const history = useHistory();
 
-  const [cartItems] = useContext(CartItemsContext);
+  // const [cartItems] = useContext(CartItemsContext);
+  // const cartItemsCount = cartItems.length;
 
-  const cartItemsCount = cartItems.length;
+  // const { cartItems } = useCartItemsStore();
+  // const cartItemsCount = cartItems.length;
+
+  const cartItemsCount = useCartItemsStore(store => store.cartItems.length);
 
   return (
     <>
