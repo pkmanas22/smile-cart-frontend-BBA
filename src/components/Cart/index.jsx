527d4c9f57fe6ef5fef8571f12667bcade0324a5
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-05-07 12:47:58 +0530

    refactor: Utilized babel-preset-neeto to access Zustand store properties

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -15,10 +15,11 @@ const Cart = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const { cartItems, setSelectedQuantity } = useCartItemsStore();
+  // const { cartItems, setSelectedQuantity } = useCartItemsStore();
+  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
 
   const slugs = keys(cartItems);
-  console.log(slugs);
+  // console.log(slugs);
 
   const fetchProducts = async () => {
     try {
