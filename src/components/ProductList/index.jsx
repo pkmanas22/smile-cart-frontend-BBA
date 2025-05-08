a419be98e25957243c9ad05226e73c413ef71842
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-05-08 11:55:34 +0530

    feat: add pagination to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,23 +4,28 @@ import { Header, PageLoader } from "components/common";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty } from "ramda";
 import { useTranslation } from "react-i18next";
 import withTitle from "utils/withTitle";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const [searchKey, setSearchKey] = useState("");
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
 
   const { t } = useTranslation();
 
   const debouncedSearchKey = useDebounce(searchKey);
 
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
-    searchTerm: debouncedSearchKey,
-  });
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts({
+      searchTerm: debouncedSearchKey,
+      page: currentPage,
+      pageSize: DEFAULT_PAGE_SIZE,
+    });
 
   if (isLoading) return <PageLoader />;
 
@@ -35,7 +40,10 @@ const ProductList = () => {
             prefix={<Search />}
             type="search"
             value={searchKey}
-            onChange={e => setSearchKey(e.target.value)}
+            onChange={e => {
+              setSearchKey(e.target.value);
+              setCurrentPage(DEFAULT_PAGE_INDEX);
+            }}
           />
         }
       />
@@ -48,6 +56,14 @@ const ProductList = () => {
           ))}
         </div>
       )}
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={page => setCurrentPage(page)}
+          pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
+      </div>
     </div>
   );
 };
