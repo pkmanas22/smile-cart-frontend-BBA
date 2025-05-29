462ae6623bc18a2ff6c3773688bd5efa43a7244a
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-05-29 15:23:13 +0530

    docs: add business logic for buildUrl() util

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,9 +2,26 @@ import { keysToSnakeCase } from "neetocist";
 import { stringify } from "qs";
 import { toPairs, omit, pipe, isEmpty } from "ramda";
 
-// route = "/products/:slug"
-// {slug: "cotton-shirt"}
+/* const route = "https://courses.bigbinaryacademy.com/:course";
+  const params = {
+    course: "learn-react",
+    name: "ma nas",
+    age: 21,
+    userId: 35,
+  }; */
+
 export const buildUrl = (route, params) => {
+  /*
+   * convert to key-value pairs of params & go through each pair
+   * if the route contains the key, that means, that is parameter for dynamic routing
+   * then replace that key with actual value
+   * add that key to a array for omitting it later
+   * create a new object by omitting that key - use omit() from lambda
+   * convert to snake cases - use keysToSnakeCase() from neetocist
+   * convert to string - use stringify() from qs, stringify() returns a string which is separated by ampersand (&)
+   * for write in one line - use pipe() from lambda, pipe(...)(obj)
+   * return the updated url according to query params
+   */
   const placeholders = [];
   toPairs(params).forEach(([key, value]) => {
     if (route.includes(`:${key}`)) {
@@ -12,6 +29,18 @@ export const buildUrl = (route, params) => {
       route = route.replace(`:${key}`, encodeURIComponent(value));
     }
   });
+
+  /*
+  console.log(placeholders); // [ 'course' ]
+
+  const queries = omit(placeholders, params); // { name: 'ma nas', age: 21, userId: 35 }
+
+  const snakeCaseParams = keysToSnakeCase(queries); // { name: 'ma nas', age: 21, user_id: 35 }
+
+  const stringified = stringify(snakeCaseParams); // 'name=ma%20nas&age=21&user_id=35'
+
+  'https://courses.bigbinaryacademy.com/learn-react?search=ma%20nas&age=21&user_id=35'
+  */
 
   const queryParams = pipe(
     omit(placeholders),
