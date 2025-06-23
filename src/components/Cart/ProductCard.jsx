1b0e0a2a5f4353f3b79f043888a71c0fa9c0a6aa
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-06-23 11:46:59 +0530

    Added link for specific product from cart page

--- src/components/Cart/ProductCard.jsx
+++ src/components/Cart/ProductCard.jsx
@@ -4,7 +4,10 @@ import ProductQuantity from "components/common/ProductQuantity";
 import { Delete } from "neetoicons";
 import { Alert, Typography } from "neetoui";
 import { Trans, useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import routes from "routes";
 import useCartItemsStore from "stores/useCartItemsStore";
+import { buildUrl } from "utils/url";
 
 const ProductCard = ({ slug, imageUrl, offerPrice, mrp, name }) => {
   const [shouldShowDeleteAlert, setShouldShowDeleteAlert] = useState(false);
@@ -19,9 +22,11 @@ const ProductCard = ({ slug, imageUrl, offerPrice, mrp, name }) => {
       <div className="flex w-full items-center space-x-5">
         <img alt={name} height={80} src={imageUrl} width={80} />
         <div className="flex-grow space-y-1">
-          <Typography className="mb-2" style="h4" weight="bold">
-            {name}
-          </Typography>
+          <Link to={buildUrl(routes.products.show, { slug })}>
+            <Typography className="mb-2" style="h4" weight="bold">
+              {name}
+            </Typography>
+          </Link>
           <Typography style="body2">{t("mrp", { mrp })}</Typography>
           {t("offerPrice", { offerPrice })}
         </div>
